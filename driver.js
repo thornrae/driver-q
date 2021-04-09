6960dbdf23dbfdd1d317ad3ceb7a49ae716d19de
Taylor Thornton <thornrae@gmail.com>
2021-04-09 10:10:13 -0700

    queue server implemneted

--- driver.js
+++ driver.js
@@ -6,12 +6,15 @@ const host = 'http://localhost:3000';
 
 io.connect(host);
 
+const faker = require('faker');
+
 const capsConnection = io.connect(`${host}/caps`);
 
 require('dotenv').config();
 
 capsConnection.on('pickup', pickUp);
 capsConnection.on('in-transit', delivered);
+capsConnection.on('received', removed)
 
 function pickUp(payload){
   setTimeout( () => {
@@ -23,6 +26,18 @@ function pickUp(payload){
 function delivered(payload){
   setTimeout( () => {
     console.log(`DRIVER: delivered ${payload.storeName} order #: ${payload.orderId}`);
+
+    // let messageId = faker.datatype.number();
+
     capsConnection.emit('delivered', payload)
+    console.log('delivered payload', payload)
+  
   }, 3000)
+
+
+}
+//is this the right place to emmit recieved? 
+
+function removed(payload){
+  console.log('order has been removed from q')
 }
